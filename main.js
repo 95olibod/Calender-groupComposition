6764eb725760398b389471f06b98bab3f249f626
Olivia Bodén <Olivia.annemaria@hotmail.com>
2021-06-17 10:07:40 +0200

    added todo.js and som data in main.js

---
+++ main.js
@@ -0,0 +1,20 @@
+window.addEventListener('load', main);
+
+const state = {
+    todos: [{
+        text: "stääääääääda",
+        date: new Date("2021-06-17")
+    }, {
+        text: "stääääääääda",
+        date: new Date("2021-06-17")
+        
+    }, {
+        text: "stääääääääda",
+        date: new Date("2021-06-17")
+    }],
+    selectedDate: undefined
+}
+
+function main () {
+    createTodo();
+}
